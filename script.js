630409620cbb1630491c413773685c9d97490d86
ashan <indrajee79@gmail.com>
2022-11-05 03:21:53 +0530

    first js lines

---
+++ script.js
@@ -0,0 +1,9 @@
+const game = document.getElementById('game');
+const scoreDisplay = document.getElementById('score');
+
+const film = 11;
+const levels = ['easy', 'medium', 'hard'];
+
+function addGenre() {
+    const column = document.createElement('div')
+}
