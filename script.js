1d0bc630f3ae4cfe120ee715110d43814ccc9bd2
ashan <indrajee79@gmail.com>
2022-11-06 12:57:44 +0530

    linking the api

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ const levels = ['easy', 'medium', 'hard'];
 function addGenre() {
     const column = document.createElement('div')
     column.classList.add('genre-column')
+    column.innerHTML = 'this is a genre'
+    game.append(column)
+
+    levels.forEach(level => {
+        fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level}&type=boolean`)
+    })
 }
+
+addGenre()
